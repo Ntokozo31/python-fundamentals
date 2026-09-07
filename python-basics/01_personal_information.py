40611886df2c5e8e69090c4e77966b849005f355
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-07 18:13:52 +0200

    Added personal information file

---
+++ python-basics/01_personal_information.py
@@ -0,0 +1,14 @@
+""""
+This program stores and displays
+basic personal information
+"""
+
+full_name = "Ntokozo Dube"
+
+height = 1.7
+
+language = "English"
+
+print(full_name)
+print(height)
+print(language)
