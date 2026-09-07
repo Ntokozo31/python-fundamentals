e08437aa91f577c1a7e80729ccdd4b14b3b8b7ff
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-07 18:15:44 +0200

    Added basic arithmetic file

---
+++ python-basics/02_basic_arithmetic.py
@@ -0,0 +1,16 @@
+"""
+This program calculates and print results of
+addition, subtration, normal division, integer division
+and modulo
+"""
+
+a = 25
+
+b = 7
+
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - 2}")
+print(f"Multiplication: {a * b}")
+print(f"Normal division: {a / b}")
+print(f"Integer division: {a // b}")
+print(f"Modulo: {a % b}")
