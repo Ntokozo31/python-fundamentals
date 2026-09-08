cf2a844bd869a26e3479aa66161803f2e0a82e3a
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-08 17:14:19 +0200

    Added file that calculates rectangle

---
+++ python-basics/03_rectangle.py
@@ -0,0 +1,15 @@
+"""
+This program calculates ractangle on a given
+length and width 
+"""
+
+length = 15
+
+width = 5
+
+area = length * width
+
+parameter = 2 * (length + width)
+
+print(f"Area: {area}")
+print(f"Parameter: {parameter}")
