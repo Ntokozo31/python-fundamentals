6acf63703be6f2c52052860553ef9e0e25b122fe
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-09 14:42:56 +0200

    Added a file that compare two numbers

---
+++ python-basics/06_number_comparison.py
@@ -0,0 +1,16 @@
+"""
+This program compares two given numbers
+and prints whether the first number is
+greater, less then or equal to second
+"""
+
+first_number = 200
+
+second_number = 1000
+
+if first_number > second_number:
+    print("First number is greater then second number.")
+elif first_number < second_number:
+    print("First number is less then second number.")
+else:
+    print("First number is equal to second number.")
