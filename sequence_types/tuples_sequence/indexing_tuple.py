7696f507abd87f018057a209b8ea4db4a0b59d26
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 18:10:03 +0200

    Added file that prints first, second and last items using indexing

---
+++ sequence_types/tuples_sequence/indexing_tuple.py
@@ -0,0 +1,10 @@
+"""
+This program create a tuple: `(10, 20, 30, 40, 50)`  
+Print the first item, the third item, and the last item (use negative indexing for the last one).
+"""
+
+numbers = (10, 20, 30, 40, 50)
+
+print(numbers[0])
+print(numbers[2])
+print(numbers[-1])
