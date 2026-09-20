0613c388c287031edab45103f28774bae371ad31
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 16:30:34 +0200

    Added file that explains different between homogeneouse and heterogeneouse

---
+++ sequence_types/lists_sequence/explain_homogeneous_heterogeneous.py
@@ -0,0 +1,13 @@
+"""
+1. Homogenouse sequance-> It's a sequance that has the same type
+
+2. Heterogenouse sequance -> it's a sequance that has different type
+"""
+
+# Examples
+
+# Homogeneouse list
+homogeneouse_list = [10, 20, 30, 40]
+
+# Heterogenouse list
+heterogeneouse_list = ["Ntokozo", 20.22, 10, True]
