474a55339287e453d3dd803eec650ae64f7c4b9c
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 18:18:59 +0200

    Added file that has IndexError for observent

---
+++ sequence_types/tuples_sequence/index_error.py
@@ -0,0 +1,8 @@
+"""
+This program create a tuple with 4 elements.  
+Try to access an index that does not exist and observe the error.
+"""
+
+numbers = (5, 10, 15, 20, 25, 30, 35, 40)
+
+print(numbers[9])
