f6ef52dad01ef447802beff2624c5e7616b87f23
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 16:45:38 +0200

    Added file that create and print tuple and its type

---
+++ sequence_types/tuples_sequence/create_and_print_tuple.py
@@ -0,0 +1,9 @@
+"""
+This program create a tuple of 4 numbers using parentheses.  
+Print the tuple and its type.
+"""
+
+numbers = (10, 20, 30, 40)
+
+print(f"Tuple: {numbers}")
+print(type(numbers))
