ac8a171a58d94dc4ff7069944fe91d2dddc54cf6
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 19:13:43 +0200

    Added file that creates tuple without parantesis (using only commas) and it prints it type

---
+++ sequence_types/tuples_sequence/tuple_without_parantesis.py
@@ -0,0 +1,9 @@
+"""
+This program Create a tuple **without** using parentheses (using only commas).  
+Print the tuple and its type.
+"""
+
+names = "Ntokozo", "Syabonga", "Bob", "John"
+
+print(f"Tuple: {names}")
+print(type(names))
