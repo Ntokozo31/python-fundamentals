37fa0397626be93b2c697d5ee3ee17bca9c6071f
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 16:49:25 +0200

    Added file that prints heterogeneouse tuple

---
+++ sequence_types/tuples_sequence/heterogeneouse_tuple.py
@@ -0,0 +1,9 @@
+"""
+This program create a heterogeneous tuple that contains a 
+string, an integer, a float, and a boolean.  
+Print the tuple.
+"""
+
+mixed = ("Ntokozo", 10, 44.5, True)
+
+print(f"Tuple: {mixed}")
