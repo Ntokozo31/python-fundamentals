4647a7da6c52a7ac89f26a2e3545406ff7239d6d
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-20 17:57:20 +0200

    Added file that print empty tuple and it length

---
+++ sequence_types/tuples_sequence/empty_tuple.py
@@ -0,0 +1,9 @@
+"""
+This program create an empty tuple in the correct way.  
+Print the tuple and its length.
+"""
+
+empty_tuple = ()
+
+print(f"Empty Tuple: {empty_tuple}")
+print(f"Length of an empty tuple: {len(empty_tuple)}")
