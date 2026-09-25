0df9bb74fe946b9feb7f7210717c12a1fa0a80f7
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-25 15:51:23 +0200

    Added file that replace the middle two colours with string orange and upack it contents item by item

---
+++ sequence_types/manipulating_sequences/colours.py
@@ -0,0 +1,11 @@
+"""
+This program replace the middle two colours with the string `"orange"`.  
+Observe and print what happens to the list.
+"""
+
+
+colours = ["red", "green", "blue", "yellow"]
+
+colours[1:3] = "orange"
+
+print(colours)
