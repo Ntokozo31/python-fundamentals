99be8e9be87e38697543d58e4cd3c5e23184d2d3
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-25 15:12:29 +0200

    Added file that changes temperature for day2 as it was recorded incorretly

---
+++ sequence_types/manipulating_sequences/daily_temperatures.py
@@ -0,0 +1,11 @@
+"""
+This program changes temperature for day 2 as
+it was recorded incorrectly, and print the updated list.
+"""
+
+
+temperatures = [18, 21, 19, 23, 25]
+
+temperatures[1] = 22
+
+print(f"Temperatures: {temperatures}")
