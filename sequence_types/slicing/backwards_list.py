d7300711ed442c81a4a6475b47816b77886b47d3
Ntokozo31 <dubentokozo45@yahoo.com>
2026-09-25 10:40:16 +0200

    Added file that prints elements from index 8 down to index 3 moving backwards

---
+++ sequence_types/slicing/backwards_list.py
@@ -0,0 +1,9 @@
+"""
+This program Create a list of numbers from 1 to 10.  
+Print the elements from index 8 down to index 3 (moving backwards).
+"""
+
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print(f"Backwards list {numbers[8:3:-1]}")
